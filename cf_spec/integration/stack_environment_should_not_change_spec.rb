1e70a65a454fa8076ea282f73710adaacee38f49
Rasheed Abdul-Aziz <pair+squeedee@pivotal.io>
2014-11-07 17:11:40 -0500

    Add a regression test for stack env variable
    [#82289072]

---
+++ cf_spec/integration/stack_environment_should_not_change_spec.rb
@@ -0,0 +1,20 @@
+$: << 'cf_spec'
+require 'cf_spec_helper'
+
+describe 'Stack environment should not change' do
+  let(:app_name) { 'sinatra_web_app' }
+
+  specify do
+    app = Machete.deploy_app(app_name)
+    expect(app).to be_running
+
+    Machete.push(app)
+    expect(app).to be_running
+
+    expect(app).to_not have_logged 'Changing stack from'
+
+    browser = Machete::Browser.new(app)
+    browser.visit_path('/')
+    expect(browser).to have_body('Hello world!')
+  end
+end
