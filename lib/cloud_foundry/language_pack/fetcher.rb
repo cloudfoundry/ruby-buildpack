458b9ad86d872874645ff8438f96df5d3b9b4ede
Colin Jackson and Rasheed Abdul-Aziz <pair+crawsible+squeedee@pivotal.io>
2015-02-23 16:33:39 -0500

    Report a readable error when a dependency is missing in translate_dependency_url

--- lib/cloud_foundry/language_pack/fetcher.rb
+++ lib/cloud_foundry/language_pack/fetcher.rb
@@ -12,6 +12,11 @@ module LanguagePack
       url = rendered_command.match(URI.regexp)[0]
       bin_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "compile-extensions", "bin"))
       translated_url = `#{bin_path}/translate_dependency_url #{url}`.chomp
+      if $?.exitstatus != 0
+        puts("Could not get translated url, exited with: #{translated_url}")
+        exit 1
+      end
+
       rendered_command.sub(url, translated_url)
     end
   end
