ae3c3220d5528d8a021149db036cbfd79457ae0a
JT Archie <jtarchie@gmail.com>
2015-04-14 11:28:21 -0400

    upgraded node JS to 0.12.2 [#90697226]
    Signed-off-by: Sai To Yeung <saitoyeung@gmail.com>
    Signed-off-by: Rasheed Abdul-Aziz <squeedee@gmail.com>

---
+++ cf_spec/unit/helpers/node_installer_spec.rb
@@ -0,0 +1,28 @@
+require 'cf_spec_helper'
+
+describe LanguagePack::NodeInstaller do
+  describe '#install' do
+    context 'with different stacks' do
+      let(:installer) { LanguagePack::NodeInstaller.new('stack') }
+
+      before do
+        allow(FileUtils).to receive(:mv)
+        allow(FileUtils).to receive(:rm_rf)
+        allow_any_instance_of(LanguagePack::Fetcher).to receive(:fetch_untar)
+      end
+
+      it 'always executes the modern Fetcher' do
+        expect_any_instance_of(LanguagePack::Fetcher).to receive(:fetch_untar).
+          with('node-v0.12.2-linux-x64.tar.gz', 'node-v0.12.2-linux-x64/bin/node')
+
+        installer.install
+      end
+
+      it 'moves the node binary to the current path' do
+        expect(FileUtils).to receive(:mv).with('node-v0.12.2-linux-x64/bin/node', '.')
+
+        installer.install
+      end
+    end
+  end
+end
