f61ca57cf1289fae1fd337db7328714cd2903884
JT Archie <jtarchie@gmail.com>
2015-04-17 15:34:56 -0400

    Regression test for rails_log_stdout plugin on Rails 3
        [#90697232]
    
    Signed-off-by: Colin Jackson <colin.s.jackson90@gmail.com>

--- cf_spec/fixtures/rails3_mri_193/config/environment.rb
+++ cf_spec/fixtures/rails3_mri_193/config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Rails3Mri193::Application.initialize!
+
+Rails.logger.info 'Logging is being redirected to STDOUT with rails_log_stdout plugin'
--- cf_spec/integration/deploy_a_rails3_app_spec.rb
+++ cf_spec/integration/deploy_a_rails3_app_spec.rb
@@ -1,22 +1,37 @@
 require 'cf_spec_helper'
 
 describe 'Rails 3 App' do
-  subject(:app) { Machete.deploy_app(app_name, with_pg: true) }
-  let(:app_name) { 'rails3_mri_193' }
-  let(:browser) { Machete::Browser.new(app) }
-
-  after do
-    Machete::CF::DeleteApp.new.execute(app)
+  before(:all) do
+    @app = Machete.deploy_app('rails3_mri_193', with_pg: true)
+    expect(@app).to be_running
   end
 
-  specify do
-    expect(app).to be_running
+  after(:all) do
+    Machete::CF::DeleteApp.new.execute(@app)
+  end
 
+  specify 'the app can be visited in the browser' do
+    browser = Machete::Browser.new(@app)
     browser.visit_path('/')
     expect(browser).to have_body('hello')
+  end
 
-    expect(app).to have_file('app/vendor/plugins/rails3_serve_static_assets/init.rb')
-    expect(app).to have_file('app/vendor/plugins/rails_log_stdout/init.rb')
-    expect(app.host).not_to have_internet_traffic if Machete::BuildpackMode.offline?
+  context 'the app did not include the static asset or logging gems from Heroku' do
+    specify 'the rails 3 plugins are installed automatically' do
+      expect(@app).to have_file 'app/vendor/plugins/rails3_serve_static_assets/init.rb'
+      expect(@app).to have_file 'app/vendor/plugins/rails_log_stdout/init.rb'
+    end
+  end
+
+  context 'a cached buildpack', if: Machete::BuildpackMode.offline? do
+    specify 'has not internet traffic' do 
+      expect(@app.host).not_to have_internet_traffic
+    end
+  end
+
+  context 'we include a rails logger message in the initializer' do
+    specify 'the log message is visible in the cf cli app logging' do
+      expect(@app).to have_logged 'Logging is being redirected to STDOUT with rails_log_stdout plugin'
+    end
   end
 end
