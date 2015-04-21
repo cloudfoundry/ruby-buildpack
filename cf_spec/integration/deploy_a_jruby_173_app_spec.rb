70388c4fa65221620eb6e9ffecebe9ad4e133ed7
JT Archie <jtarchie@gmail.com>
2015-04-21 17:00:48 -0400

    updated to latest openjdk1.8
    [#90697236]
    
    Signed-off-by: Sai To Yeung <saitoyeung@gmail.com>

---
+++ cf_spec/integration/deploy_a_jruby_173_app_spec.rb
@@ -0,0 +1,27 @@
+require 'cf_spec_helper'
+
+describe 'JRuby 1.7.3 App' do
+  before(:all) do
+    @app = Machete.deploy_app('sinatra_jruby_web_app', with_pg: true)
+    expect(@app).to be_running
+  end
+
+  after(:all) do
+    Machete::CF::DeleteApp.new.execute(@app)
+  end
+
+  specify 'the buildpack logged it installed a specific version of JRuby' do
+    expect(@app).to have_logged 'Installing JVM'
+    expect(@app).to have_logged 'ruby-1.8.7-jruby-1.7.8'
+  end
+
+  specify 'the OpenJDK runs properly' do
+    expect(@app).not_to have_logged 'OpenJDK 64-Bit Server VM warning'
+  end
+
+  context 'a cached buildpack', if: Machete::BuildpackMode.offline? do
+    specify 'has no internet traffic' do
+      expect(@app.host).not_to have_internet_traffic
+    end
+  end
+end
