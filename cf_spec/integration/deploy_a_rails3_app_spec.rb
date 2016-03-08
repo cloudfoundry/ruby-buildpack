0d1584f4d2037e2d05754d9fefd5800442b61d8c
James Wen <jrw2175@columbia.edu>
2016-03-08 14:46:50 -0500

    Make relative paths absolute in integration specs
    - Diego SSH daemon changes results in $HOME consistency with app runtime
    - https://github.com/cloudfoundry-incubator/diego-release/releases/tag/v0.1455.0
    
    Signed-off-by: John Shahid <jvshahid@gmail.com>

--- cf_spec/integration/deploy_a_rails3_app_spec.rb
+++ cf_spec/integration/deploy_a_rails3_app_spec.rb
@@ -20,8 +20,8 @@ describe 'Rails 3 App' do
 
   context 'the app did not include the static asset or logging gems from Heroku' do
     specify 'the rails 3 plugins are installed automatically' do
-      expect(@app).to have_file 'app/vendor/plugins/rails3_serve_static_assets/init.rb'
-      expect(@app).to have_file 'app/vendor/plugins/rails_log_stdout/init.rb'
+      expect(@app).to have_file '/app/vendor/plugins/rails3_serve_static_assets/init.rb'
+      expect(@app).to have_file '/app/vendor/plugins/rails_log_stdout/init.rb'
     end
   end
 
