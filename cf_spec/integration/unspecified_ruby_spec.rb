0373274c02ed6cc98c01490d031d47eb35a34aee
James Wen <jrw2175@columbia.edu>
2016-04-27 16:11:18 -0400

    Make ruby 2.2.5 the default ruby version
    [#118363433]

---
+++ cf_spec/integration/unspecified_ruby_spec.rb
@@ -0,0 +1,17 @@
+require 'cf_spec_helper'
+
+describe 'CF Ruby Buildpack' do
+  before(:all) do
+    @app = Machete.deploy_app('unspecified_ruby')
+  end
+
+  after(:all) { Machete::CF::DeleteApp.new.execute(@app) }
+
+  it 'starts' do
+    expect(@app).to be_running
+  end
+
+  it 'uses the default ruby version' do
+    expect(@app).to have_logged 'Using Ruby version: ruby-2.2.5'
+  end
+end
