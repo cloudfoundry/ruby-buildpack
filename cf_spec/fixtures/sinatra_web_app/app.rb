9abfa85f470ea086aafac419da778eaf3927c30b
Mike Dalessio <mike.dalessio@gmail.com>
2016-09-29 10:30:53 -0400

    add an /env endpoint to sinatra_web_app

--- cf_spec/fixtures/sinatra_web_app/app.rb
+++ cf_spec/fixtures/sinatra_web_app/app.rb
@@ -8,3 +8,12 @@ end
 get '/yaml' do
   '<pre>' + YAML.load("{foo: [bar, baz, quux]}").to_yaml + '</pre>'
 end
+
+get '/env' do
+  rval = "<pre>"
+  ENV.keys.sort.each do |key|
+    rval += "#{key}: #{ENV[key]}\n"
+  end
+  rval += "</pre>"
+  rval
+end
