8db5d3863bd63a1f5da830427dda663c8f6616a5
Jacques Chester <jchester@pivotal.io>
2016-11-30 17:26:50 -0500

    Force RSpec to use coloured output on Concourse
    [ci skip]
    
    [#133046105]

--- cf_spec/cf_spec_helper.rb
+++ cf_spec/cf_spec_helper.rb
@@ -9,6 +9,9 @@ require 'cloud_foundry/language_pack/extensions'
 Machete.logger = Machete::Logger.new("log/integration.log")
 
 RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+
   config.filter_run_excluding :cached => ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding :uncached => ENV['BUILDPACK_MODE'] == 'cached'
 end
