79302fc10cf6f617aa7edc72b422965e08ff52b7
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2017-01-17 16:50:53 -0500

    Revert "Merge pull request #516 from heroku/testpack"
    This reverts commit 857d13c6903f2378b2a41d29cdd8ad0237ceaec9, reversing
    changes made to 37ed1882b944afa350aec4aec83e258ccb2bc39e.

--- lib/language_pack/rails4.rb
+++ lib/language_pack/rails4.rb
@@ -30,7 +30,7 @@ class LanguagePack::Rails4 < LanguagePack::Rails3
     end
   end
 
-  def build_bundler(default_bundle_without)
+  def build_bundler
     instrument "rails4.build_bundler" do
       super
     end
