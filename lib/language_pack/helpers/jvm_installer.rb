2a9b4be624ed4ef20f97f37ae4c3d529bb9863dc
Dave Goddard <dave@goddard.id.au>
2017-03-14 10:48:01 -0400

    Simplify JDK version logic
    - Remove cedar
    
    [#141519643]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- lib/language_pack/helpers/jvm_installer.rb
+++ lib/language_pack/helpers/jvm_installer.rb
@@ -6,11 +6,7 @@ class LanguagePack::JvmInstaller
   SYS_PROPS_FILE  = "system.properties"
   JVM_BUCKET      = "https://lang-jvm.s3.amazonaws.com"
   JVM_BASE_URL    = "#{JVM_BUCKET}/jdk"
-  JVM_1_9_PATH    = "openjdk1.9-latest.tar.gz"
   JVM_1_8_PATH    = "openjdk1.8-latest.tar.gz"
-  JVM_1_7_PATH    = "openjdk1.7-latest.tar.gz"
-  JVM_1_7_25_PATH = "openjdk1.7.0_25.tar.gz"
-  JVM_1_6_PATH    = "openjdk1.6-latest.tar.gz"
 
   PG_CONFIG_JAR   = "pgconfig.jar"
 
@@ -37,27 +33,7 @@ class LanguagePack::JvmInstaller
       return
     end
 
-    jvm_version = system_properties['java.runtime.version']
-    case jvm_version
-    when "1.9", "9"
-      fetch_env_untar('JDK_URL_1_9') || fetch_untar(JVM_1_9_PATH, "openjdk-9")
-    when "1.7", "7"
-      fetch_env_untar('JDK_URL_1_7') || fetch_untar(JVM_1_7_PATH, "openjdk-7")
-    when "1.6", "6"
-      fetch_env_untar('JDK_URL_1_6') || fetch_untar(JVM_1_6_PATH, "openjdk-6")
-    when nil
-      if @stack == "cedar"
-        if forced || Gem::Version.new(jruby_version) >= Gem::Version.new("1.7.4")
-          fetch_untar(JVM_1_7_PATH, "openjdk-7")
-        else
-          fetch_untar(JVM_1_7_25_PATH)
-        end
-      else
-      fetch_env_untar('JDK_URL_1_8') || fetch_untar(JVM_1_8_PATH, "openjdk-8")
-      end
-    else
-      fetch_untar("openjdk#{jvm_version}.tar.gz", "openjdk-#{jvm_version}")
-    end
+    fetch_untar(JVM_1_8_PATH, "openjdk-8")
 
     bin_dir = "bin"
     FileUtils.mkdir_p bin_dir
@@ -81,17 +57,6 @@ Failed to download JVM: #{jvm_path}
 
 If this was a custom version or URL, please check to ensure it is correct.
 EOF
-  end
-
-  def fetch_env_untar(key)
-    val = env(key)
-    return false unless val
-    path = Pathname.new(val)
-    jvm_version = path.basename.to_s
-    base_url = val[0, val.index(jvm_version)]
-    @fetcher =  LanguagePack::Fetcher.new(base_url)
-    fetch_untar(jvm_version)
-    true
   end
 
   def install_pgconfig_jar
