82c6d6544a54de0a77d83a40f23030d4519f4513
Sam Smith <sesmith177@gmail.com>
2017-05-08 11:17:56 -0400

    Use '_' instead of '-' in .profile.d script names
    [#144964891]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- cf_spec/integration/specified_ruby_version_spec.rb
+++ cf_spec/integration/specified_ruby_version_spec.rb
@@ -1,4 +1,5 @@
 require 'cf_spec_helper'
+require 'open3'
 
 describe 'CF Ruby Buildpack' do
   before(:all) do
@@ -13,5 +14,16 @@ describe 'CF Ruby Buildpack' do
 
   it 'uses the specified ruby version' do
     expect(@app).to have_logged 'Using Ruby version: ruby-2.2.7'
+  end
+
+  context 'running a task' do
+    before { skip_if_no_run_task_support_on_targeted_cf }
+
+    it 'can find the specifed ruby in the container' do
+      expect(@app).to be_running
+
+      Open3.capture2e('cf','run-task', 'specified_ruby_version', 'echo "RUNNING A TASK: $(ruby --version)"')[1].success? or raise 'Could not create run task'
+      expect(@app).to have_logged(/RUNNING A TASK: ruby 2\.2\.7/)
+    end
   end
 end
