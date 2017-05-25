17a06932ee7920de6fdfc839971b30f55821fbeb
Dave Goddard <dave@goddard.id.au>
2017-05-25 09:24:58 -0400

    Remove unneeded code [#145768065]
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- lib/language_pack/helpers/bundler_wrapper.rb
+++ lib/language_pack/helpers/bundler_wrapper.rb
@@ -42,13 +42,6 @@ class LanguagePack::Helpers::BundlerWrapper
   def clean
     ENV['BUNDLE_GEMFILE'] = @orig_bundle_gemfile
     FileUtils.remove_entry_secure(@bundler_tmp) if Dir.exist?(@bundler_tmp)
-
-    if LanguagePack::Ruby::BUNDLER_VERSION  == "1.7.12"
-      # Hack to cleanup after pre 1.8 versions of bundler. See https://github.com/bundler/bundler/pull/3277/
-      Dir["#{Dir.tmpdir}/bundler*"].each do |dir|
-        FileUtils.remove_entry_secure(dir) if Dir.exist?(dir) && File.stat(dir).writable?
-      end
-    end
   end
 
   def has_gem?(name)
