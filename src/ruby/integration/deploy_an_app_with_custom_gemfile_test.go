d5999078a5f2c0b141ca0efcd80619ed66967a16
Dave Goddard <dave@goddard.id.au>
2017-09-14 11:22:31 -0400

    Add ruby 2.4.2, 2.3.5, 2.2.8. Remove 2.4.0, 2.3.3, 2.2.6
    [#151087653]

--- src/ruby/integration/deploy_an_app_with_custom_gemfile_test.go
+++ src/ruby/integration/deploy_an_app_with_custom_gemfile_test.go
@@ -25,6 +25,6 @@ var _ = Describe("App with custom Gemfile", func() {
 
 	It("uses the version of ruby specified in Gemfile-APP", func() {
 		PushAppAndConfirm(app)
-		Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 2.2.6"))
+		Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 2.2.8"))
 	})
 })
--- src/ruby/integration/specified_ruby_version_test.go
+++ src/ruby/integration/specified_ruby_version_test.go
@@ -22,7 +22,7 @@ var _ = Describe("CF Ruby Buildpack", func() {
 		PushAppAndConfirm(app)
 
 		By("uses the specified ruby version", func() {
-			Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 2.2.7"))
+			Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 2.2.8"))
 		})
 
 		By("running a task", func() {
@@ -34,7 +34,7 @@ var _ = Describe("CF Ruby Buildpack", func() {
 				_, err := app.RunTask(`echo "RUNNING A TASK: $(ruby --version)"`)
 				Expect(err).ToNot(HaveOccurred())
 
-				Eventually(func() string { return app.Stdout.String() }, 10*time.Second).Should(ContainSubstring("RUNNING A TASK: ruby 2.2.7"))
+				Eventually(func() string { return app.Stdout.String() }, 10*time.Second).Should(ContainSubstring("RUNNING A TASK: ruby 2.2.8"))
 			})
 		})
 	})
