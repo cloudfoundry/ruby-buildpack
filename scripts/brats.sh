6462f8a935d016d35668852f6a531db087e5d49f
Dave Goddard <dave@goddard.id.au>
2017-11-10 12:13:55 -0500

    Update scripts/brats to match new pattern

--- scripts/brats.sh
+++ scripts/brats.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export ROOT=$(dirname $(readlink -f ${BASH_SOURCE%/*}))
-if [ ! -f "$ROOT/.bin/ginkgo" ]; then
-  echo "Installing ginkgo"
-  (cd "$ROOT/src/ruby/vendor/github.com/onsi/ginkgo/ginkgo/" && go install)
-fi
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+source .envrc
+./scripts/install_tools.sh
 
 GINKGO_NODES=${GINKGO_NODES:-3}
 GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-2}
 
-cd $ROOT/src/ruby/brats
+cd src/*/brats
+
+echo "Run Buildpack Runtime Acceptance Tests"
 ginkgo -r --flakeAttempts=$GINKGO_ATTEMPTS -nodes $GINKGO_NODES
