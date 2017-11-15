ad1b9d99f7908b553ebe4c43e78e29816251646d
Dave Goddard <dave@goddard.id.au>
2017-11-15 16:39:49 -0500

    Remove os.RemoveAll CF_HOME
    Causes delete orphaned routes to fail as we are no longer logged in
    
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

--- src/ruby/integration/integration_suite_test.go
+++ src/ruby/integration/integration_suite_test.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/blang/semver"
@@ -65,9 +64,6 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 
 var _ = SynchronizedAfterSuite(func() {
 	// Run on all nodes
-	if strings.HasPrefix(os.Getenv("CF_HOME"), "/tmp") {
-		Expect(os.RemoveAll(os.Getenv("CF_HOME"))).To(Succeed())
-	}
 }, func() {
 	// Run once
 	cutlass.RemovePackagedBuildpack(packagedBuildpack)
