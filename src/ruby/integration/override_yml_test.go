5029c0c076afe3151c50d16cc35be5157e45e9bf
Dave Goddard <dave@goddard.id.au>
2018-02-05 14:45:18 -0500

    Enable manifest apply override [#153614649]
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>

---
+++ src/ruby/integration/override_yml_test.go
@@ -0,0 +1,39 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("override yml", func() {
+	var app *cutlass.App
+	var buildpackName string
+	AfterEach(func() {
+		cutlass.DeleteBuildpack(buildpackName)
+		app = DestroyApp(app)
+	})
+
+	BeforeEach(func() {
+		buildpackName = "override_yml_" + cutlass.RandStringRunes(5)
+		Expect(cutlass.CreateOrUpdateBuildpack(buildpackName, filepath.Join(bpDir, "fixtures", "overrideyml_bp"))).To(Succeed())
+
+		app = cutlass.New(filepath.Join(bpDir, "fixtures", "with_execjs"))
+		app.Buildpacks = []string{buildpackName + "_buildpack", "ruby_buildpack"}
+	})
+
+	It("Forces nodejs from override buildpack, installs ruby from ruby buildpack", func() {
+		Expect(app.Push()).ToNot(Succeed())
+		Expect(app.Stdout.String()).To(ContainSubstring("-----> OverrideYML Buildpack"))
+		Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
+
+		Eventually(app.Stdout.String).Should(ContainSubstring("-----> Installing ruby"))
+
+		Eventually(app.Stdout.String).Should(ContainSubstring("-----> Installing node"))
+		Eventually(app.Stdout.String).Should(MatchRegexp("Copy .*/node.tgz"))
+		Eventually(app.Stdout.String).Should(ContainSubstring("Unable to install node: dependency sha256 mismatch: expected sha256 062d906c87839d03b243e2821e10653c89b4c92878bfe2bf995dec231e117bfc"))
+	})
+})
