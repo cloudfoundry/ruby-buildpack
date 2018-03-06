ba8119b87b386a475c84c35db19c7d7e967bdc77
Dave Goddard <dave@goddard.id.au>
2018-03-06 18:02:28 -0400

    Use manifest cache downloads
    [#155405429]

--- src/ruby/integration/push_app_second_time_test.go
+++ src/ruby/integration/push_app_second_time_test.go
@@ -15,15 +15,38 @@ var _ = Describe("pushing an app a second time", func() {
 
 	BeforeEach(func() {
 		app = cutlass.New(filepath.Join(bpDir, "fixtures", "sinatra"))
+		app.SetEnv("BP_DEBUG", "true")
 	})
+
+	RestoringVendorBundle := "Restoring vendor_bundle from cache"
+	DownloadRegexp := `Download \[.*/bundler\-.*\.tgz\]`
+	CopyRegexp := `Copy \[.*/bundler\-.*\.tgz\]`
 
 	It("uses the cache and runs", func() {
 		PushAppAndConfirm(app)
-		Expect(app.Stdout.String()).ToNot(ContainSubstring("Restoring vendor_bundle from cache"))
+		Expect(app.Stdout.String()).ToNot(ContainSubstring(RestoringVendorBundle))
+		if !cutlass.Cached {
+			Expect(app.Stdout.String()).To(MatchRegexp(DownloadRegexp))
+			Expect(app.Stdout.String()).ToNot(MatchRegexp(CopyRegexp))
+		}
 		Expect(app.GetBody("/")).To(ContainSubstring("Hello world!"))
 
+		app.Stdout.Reset()
 		PushAppAndConfirm(app)
-		Expect(app.Stdout.String()).To(ContainSubstring("Restoring vendor_bundle from cache"))
+		Expect(app.Stdout.String()).To(ContainSubstring(RestoringVendorBundle))
+		if !cutlass.Cached {
+			Expect(app.Stdout.String()).To(MatchRegexp(CopyRegexp))
+			Expect(app.Stdout.String()).ToNot(MatchRegexp(DownloadRegexp))
+		}
+		Expect(app.GetBody("/")).To(ContainSubstring("Hello world!"))
+
+		app.Stdout.Reset()
+		PushAppAndConfirm(app)
+		Expect(app.Stdout.String()).To(ContainSubstring(RestoringVendorBundle))
+		if !cutlass.Cached {
+			Expect(app.Stdout.String()).To(MatchRegexp(CopyRegexp))
+			Expect(app.Stdout.String()).ToNot(MatchRegexp(DownloadRegexp))
+		}
 		Expect(app.GetBody("/")).To(ContainSubstring("Hello world!"))
 	})
 })
