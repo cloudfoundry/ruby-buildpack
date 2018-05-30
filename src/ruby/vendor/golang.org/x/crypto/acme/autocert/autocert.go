410812625da249c8218f2e6906c76653e8db6aba
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2018-05-30 20:28:14 +0000

    Update libbuildpack

--- src/ruby/vendor/golang.org/x/crypto/acme/autocert/autocert.go
+++ src/ruby/vendor/golang.org/x/crypto/acme/autocert/autocert.go
@@ -146,6 +146,15 @@ type Manager struct {
 	// is EC-based keys using the P-256 curve.
 	ForceRSA bool
 
+	// ExtraExtensions are used when generating a new CSR (Certificate Request),
+	// thus allowing customization of the resulting certificate.
+	// For instance, TLS Feature Extension (RFC 7633) can be used
+	// to prevent an OCSP downgrade attack.
+	//
+	// The field value is passed to crypto/x509.CreateCertificateRequest
+	// in the template's ExtraExtensions field as is.
+	ExtraExtensions []pkix.Extension
+
 	clientMu sync.Mutex
 	client   *acme.Client // initialized by acmeClient method
 
@@ -527,7 +536,7 @@ func (m *Manager) authorizedCert(ctx context.Context, key crypto.Signer, domain 
 	if err := m.verify(ctx, client, domain); err != nil {
 		return nil, nil, err
 	}
-	csr, err := certRequest(key, domain)
+	csr, err := certRequest(key, domain, m.ExtraExtensions)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -542,17 +551,15 @@ func (m *Manager) authorizedCert(ctx context.Context, key crypto.Signer, domain 
 	return der, leaf, nil
 }
 
-// revokePending revokes all provided authorizations (passed as a map of URIs)
-func (m *Manager) revokePending(ctx context.Context, pendingAuthzURIs map[string]bool) {
-	f := func(ctx context.Context) {
-		for uri := range pendingAuthzURIs {
-			m.Client.RevokeAuthorization(ctx, uri)
-		}
+// revokePendingAuthz revokes all authorizations idenfied by the elements of uri slice.
+// It ignores revocation errors.
+func (m *Manager) revokePendingAuthz(ctx context.Context, uri []string) {
+	client, err := m.acmeClient(ctx)
+	if err != nil {
+		return
 	}
-	if waitForRevocations {
-		f(ctx)
-	} else {
-		go f(context.Background())
+	for _, u := range uri {
+		client.RevokeAuthorization(ctx, u)
 	}
 }
 
@@ -568,9 +575,21 @@ func (m *Manager) verify(ctx context.Context, client *acme.Client, domain string
 	}
 	m.tokensMu.RUnlock()
 
-	// we keep track of pending authzs and revoke the ones that did not validate.
+	// Keep track of pending authzs and revoke the ones that did not validate.
 	pendingAuthzs := make(map[string]bool)
-	defer m.revokePending(ctx, pendingAuthzs)
+	defer func() {
+		var uri []string
+		for k, pending := range pendingAuthzs {
+			if pending {
+				uri = append(uri, k)
+			}
+		}
+		if len(uri) > 0 {
+			// Use "detached" background context.
+			// The revocations need not happen in the current verification flow.
+			go m.revokePendingAuthz(context.Background(), uri)
+		}
+	}()
 
 	var nextTyp int // challengeType index of the next challenge type to try
 	for {
@@ -870,12 +889,12 @@ func (s *certState) tlscert() (*tls.Certificate, error) {
 	}, nil
 }
 
-// certRequest creates a certificate request for the given common name cn
-// and optional SANs.
-func certRequest(key crypto.Signer, cn string, san ...string) ([]byte, error) {
+// certRequest generates a CSR for the given common name cn and optional SANs.
+func certRequest(key crypto.Signer, cn string, ext []pkix.Extension, san ...string) ([]byte, error) {
 	req := &x509.CertificateRequest{
-		Subject:  pkix.Name{CommonName: cn},
-		DNSNames: san,
+		Subject:         pkix.Name{CommonName: cn},
+		DNSNames:        san,
+		ExtraExtensions: ext,
 	}
 	return x509.CreateCertificateRequest(rand.Reader, req, key)
 }
@@ -980,7 +999,4 @@ var (
 
 	// Called when a state is removed.
 	testDidRemoveState = func(domain string) {}
-
-	// make testing of revokePending synchronous
-	waitForRevocations = false
 )
