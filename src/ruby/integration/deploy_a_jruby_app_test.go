7225b324a07d6f4901ee467b5dbec5c2e588a3f8
Jackson Feeny <jacksonfeeny@gmail.com>
2018-06-21 13:36:34 -0500

    Add jruby 9.2.0.0, remove 9.1.17.0 [#157851009]
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- src/ruby/integration/deploy_a_jruby_app_test.go
+++ src/ruby/integration/deploy_a_jruby_app_test.go
@@ -25,7 +25,7 @@ var _ = Describe("JRuby App", func() {
 
 			By("the buildpack logged it installed a specific version of JRuby", func() {
 				Expect(app.Stdout.String()).To(ContainSubstring("Installing openjdk"))
-				Expect(app.Stdout.String()).To(MatchRegexp("ruby-2.3.\\d+-jruby-9.1.\\d+.0"))
+				Expect(app.Stdout.String()).To(MatchRegexp("ruby-2.3.\\d+-jruby-9.\\d+.\\d+.0"))
 				Expect(app.GetBody("/ruby")).To(MatchRegexp("jruby 2.3.\\d+"))
 			})
 
