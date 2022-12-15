a6f6c3e1ef3876ed4b61c6b2dda59f4a1fb8deaf
Arjun Sreedharan <asreedharan@vmware.com>
2022-12-15 22:12:03 +0000

    tests: update tests to support 2 stacks

--- src/ruby/integration/deploy_an_app_with_custom_gemfile_bad_dummy_gemfile_test.go
+++ src/ruby/integration/deploy_an_app_with_custom_gemfile_bad_dummy_gemfile_test.go
@@ -23,7 +23,7 @@ var _ = Describe("App with custom Gemfile and bad file named 'Gemfile'", func() 
 
 	It("uses the version of ruby specified in Gemfile-APP", func() {
 		PushAppAndConfirm(app)
-		Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 2.7"))
+		Expect(app.Stdout.String()).To(ContainSubstring("Installing ruby 3.1"))
 		Expect(app.Stdout.String()).To(ContainSubstring("Installing sinatra 2.2.0"))
 	})
 })
