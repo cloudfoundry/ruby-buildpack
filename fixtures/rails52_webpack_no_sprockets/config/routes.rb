66815df01a8c1c26defdc43d5d6f890df86c163b
Sophie Wigmore <swigmore@vmware.com>
2023-04-03 13:15:24 +0200

    Remove out-of-date and unused rails fixtures
    - the thinreports-rails3-example app was not used in any tests, and
    rails 3 is EOL
    - the rails52_webpack_no_sprockets test has been "Pending" for 2 years,
    and Rails 5 is EOL (01 Jun 2022)

--- fixtures/rails52_webpack_no_sprockets/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- fixtures/thinreports-rails3-example/app/models/task.rb
+++
@@ -1,7 +0,0 @@
-class Task < ActiveRecord::Base
-  attr_accessible :done, :due_date, :name
-
-  def state
-    done? ? 'done' : 'yet'
-  end
-end
