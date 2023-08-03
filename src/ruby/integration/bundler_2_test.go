33af824e01d6155dce01c923e86156098f4101f8
Brayan Henao <bhenao@vmware.com>
2023-08-03 15:56:25 -0400

    Fix errors with tests

--- src/ruby/integration/bundler_2_test.go
+++ src/ruby/integration/bundler_2_test.go
@@ -13,9 +13,10 @@ var _ = Describe("CF Ruby Buildpack", func() {
 	AfterEach(func() { app = DestroyApp(app) })
 
 	It("works with old version of bundler 2", func() {
-		app = cutlass.New(Fixtures("bundler_2_0_1"))
+		app = cutlass.New(Fixtures("bundler_2"))
 		PushAppAndConfirm(app)
-		Expect(app.Stdout.String()).To(ContainSubstring("Using bundler 2"))
+		Expect(app.Stdout.String()).To(MatchRegexp(`Your Gemfile.lock was bundled with bundler 2\.\d+\.\d+, which is incompatible with the current bundler version \(2\.\d+\.\d+\)`))
 		Expect(app.Stdout.String()).To(ContainSubstring(`Deleting "Bundled With" from the Gemfile.lock`))
+		Expect(app.GetBody("/")).To(ContainSubstring("Hello world!"))
 	})
 })
