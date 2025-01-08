6698e8ee165f9abf712e9d17325b36af9faba0ab
Rob Dimsdale-Zucker <rob.dimsdale-zucker@broadcom.com>
2025-01-07 16:05:40 -0800

    Remove unsupported brats tests.

--- src/ruby/brats/brats_test.go
+++ src/ruby/brats/brats_test.go
@@ -10,12 +10,9 @@ import (
 
 var _ = Describe("Ruby buildpack", func() {
 	bratshelper.UnbuiltBuildpack("ruby", CopyBrats)
-	bratshelper.DeployingAnAppWithAnUpdatedVersionOfTheSameBuildpack(CopyBrats)
-	bratshelper.StagingWithBuildpackThatSetsEOL("ruby", func(_ string) *cutlass.App {
-		return CopyBrats("3.1.x")
-	})
+
 	bratshelper.DeployAppWithExecutableProfileScript("ruby", CopyBrats)
-	bratshelper.DeployAnAppWithSensitiveEnvironmentVariables(CopyBrats)
+
 	bratshelper.ForAllSupportedVersions("ruby", CopyBrats, func(rubyVersion string, app *cutlass.App) {
 		PushApp(app)
 
