6e5ee6f646ee894e42d97865d1c56affa0a39399
ivanovac <ivanovac@gmail.com>
2026-02-04 11:24:43 +0200

    Add cflinuxfs5 support to install_go.sh and update config.json to cflinuxfs5

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -5,27 +5,42 @@ set -u
 set -o pipefail
 
 function main() {
-  # Stack check temporarily disabled for cflinuxfs5 testing
-  # if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" ]]; then
-  #   echo "       **ERROR** Unsupported stack"
-  #   echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
-  #   exit 1
-  # fi
+  echo "-----> [DEBUG] install_go.sh called, CF_STACK=${CF_STACK:-NOT_SET}"
+  
+  # Allow cflinuxfs3, cflinuxfs4, and cflinuxfs5
+  if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" && "${CF_STACK:-}" != "cflinuxfs5" ]]; then
+    echo "       **ERROR** Unsupported stack"
+    echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
+    exit 1
+  fi
 
   local version expected_sha dir
-  version="1.22.5"
-  expected_sha="ddb12ede43eef214c7d4376761bd5ba6297d5fa7a06d5635ea3e7a276b3db730"
+  if [[ "${CF_STACK}" == "cflinuxfs5" ]]; then
+    version="1.23.12"
+    expected_sha="ff5ab3b02743246897bfc65d7c205e58eabba313d6a07587bec97d812fb3c6c0"
+  else
+    version="1.22.5"
+    expected_sha="ddb12ede43eef214c7d4376761bd5ba6297d5fa7a06d5635ea3e7a276b3db730"
+  fi
   dir="/tmp/go${version}"
 
   mkdir -p "${dir}"
 
+  echo "-----> [DEBUG] install_go.sh: CF_STACK=${CF_STACK:-not set}, Go version=${version}"
+
   if [[ ! -f "${dir}/bin/go" ]]; then
     local url
-    # TODO: use exact stack based dep, after go buildpack has cflinuxfs4 support
-    #url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
-    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
+    # Use stack-based Go dependency URL
+    if [[ "${CF_STACK}" == "cflinuxfs5" ]]; then
+      echo "-----> [DEBUG] Using GitHub URL for cflinuxfs5"
+      url="https://github.com/ivanovac/go-buildpack/releases/download/v1.11.00-beta-cflinuxfs5/go_${version}_linux_x64_cflinuxfs5_${expected_sha:0:8}.tgz"
+    else
+      echo "-----> [DEBUG] Using buildpacks.cloudfoundry.org URL for ${CF_STACK}"
+      url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
+    fi
 
     echo "-----> Download go ${version}"
+    echo "-----> [DEBUG] URL: ${url}"
     curl "${url}" \
       --silent \
       --location \
